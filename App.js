e71d3a82f4f49cfe906128e94f798fda098a5ef4
Pushkar Singh <theagilecoder@gmail.com>
2020-07-24 20:55:53 +0530

    Add 3 Pomodoros component

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React, {useState, useRef, useCallback} from 'react';
-import {StyleSheet, View} from 'react-native';
-import BackgroundTimer from 'react-native-background-timer';
+import React from 'react';
+import {View} from 'react-native';
 import PushNotification from 'react-native-push-notification';
 import Header from './src/components/Header';
 import Pomodoro1 from './src/components/Pomodoro1';
 import Pomodoro2 from './src/components/Pomodoro2';
+import Pomodoro3 from './src/components/Pomodoro3';
 
 PushNotification.configure({
   onRegister: function (token) {
@@ -22,59 +22,15 @@ PushNotification.configure({
   requestPermissions: Platform.OS === 'ios',
 });
 
-const useCounter = (initialValue, ms) => {
-  const [count, setCount] = useState(initialValue);
-  const intervalRef = useRef(null);
-
-  const start = useCallback(() => {
-    if (intervalRef.current !== null) {
-      return;
-    }
-    intervalRef.current = BackgroundTimer.setInterval(() => {
-      setCount((c) => c + 1);
-    }, ms);
-  }, []);
-
-  const pause = useCallback(() => {
-    if (intervalRef.current === null) {
-      return;
-    }
-    BackgroundTimer.clearInterval(intervalRef.current);
-    intervalRef.current = null;
-  }, []);
-
-  const reset = useCallback(() => {
-    setCount(0);
-  }, []);
-
-  return {count, start, pause, reset};
-};
-
 const App = () => {
-  const {count, start, pause, reset} = useCounter(0, 500);
-
-  const setNotification = () => {
-    PushNotification.localNotificationSchedule({
-      title: 'My Notification Title', // (optional)
-      message: 'My Notification Message', // (required)
-      date: new Date(Date.now() + 1 * 5 * 1000), // in x * 1 min
-    });
-  };
-
-  const cancelNotfication = () => {
-    PushNotification.removeAllDeliveredNotifications();
-    // PushNotification.cancelAllLocalNotifications();
-  };
-
   return (
     <View>
       <Header />
       <Pomodoro1 />
       <Pomodoro2 />
+      <Pomodoro3 />
     </View>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
---
+++ src/components/Pomodoro3.js
@@ -0,0 +1,148 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import Collapsible from 'react-native-collapsible';
+import PushNotification from 'react-native-push-notification';
+import BackgroundTimer from 'react-native-background-timer';
+import useCounter from './Counter';
+import handleSeconds from './HandleSeconds';
+
+PushNotification.configure({
+  onRegister: function (token) {
+    console.log('TOKEN:', token);
+  },
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  popInitialNotification: true,
+  requestPermissions: Platform.OS === 'ios',
+});
+
+const Pomodoro3 = () => {
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleCollapse = () => setCollapsed(!collapsed);
+  const {count, start, stop, reset} = useCounter(0, 1000);
+  const [label, setLabel] = useState('');
+
+  const sendNotification = () => {
+    // Start Counter & Send notification at start of 1st Pomodoro
+    start();
+    setLabel('Work : ');
+    PushNotification.localNotification({
+      message: '1st Pomodoro started',
+    });
+
+    // Reset Counter & Send notification at end of 1st Pomodoro
+    BackgroundTimer.setTimeout(() => {
+      reset();
+      setLabel('Rest : ');
+      PushNotification.localNotification({
+        message: '1st Pomodoro finished - Take Rest',
+      });
+    }, 1500 * 1000);
+
+    // Reset Counter & Send notification at end of 1st Rest
+    BackgroundTimer.setTimeout(() => {
+      reset();
+      setLabel('Work : ');
+      PushNotification.localNotification({
+        message: 'Rest over - 2nd Pomodoro started',
+      });
+    }, 1800 * 1000);
+
+    // Reset Counter & Send notification at end of 2nd Pomodoro
+    BackgroundTimer.setTimeout(() => {
+      reset();
+      setLabel('Rest : ');
+      PushNotification.localNotification({
+        message: '2nd Pomodoro finished - Take Rest',
+      });
+    }, 3300 * 1000);
+
+    // Reset Counter & Send notification at end of 2nd Rest
+    BackgroundTimer.setTimeout(() => {
+      reset();
+      setLabel('Work : ');
+      PushNotification.localNotification({
+        message: 'Rest over - 3rd Pomodoro started',
+      });
+    }, 3600 * 1000);
+
+    // Reset Counter & Send notification at end of 3rd Pomodoro
+    BackgroundTimer.setTimeout(() => {
+      stop();
+      reset();
+      setLabel('Finished');
+      PushNotification.localNotification({
+        message: '3rd Pomodoro finished',
+      });
+    }, 5100 * 1000);
+  };
+
+  const cancelNotification = () => {
+    // Handle Notifications
+    // PushNotification.removeAllDeliveredNotifications();
+    PushNotification.cancelAllLocalNotifications();
+
+    // Handle Counter
+    stop();
+    reset();
+    setLabel('');
+  };
+
+  return (
+    <View>
+      <TouchableOpacity onPress={toggleCollapse}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>3 Pomodoros</Text>
+          <Text style={styles.counter}>{label + handleSeconds(count)}</Text>
+        </View>
+      </TouchableOpacity>
+      <Collapsible collapsed={collapsed}>
+        <View style={styles.buttonWrapper}>
+          <View style={styles.button}>
+            <Button title="Start" onPress={sendNotification} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" onPress={cancelNotification} />
+          </View>
+        </View>
+      </Collapsible>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#F1E9F5',
+    padding: 10,
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    paddingLeft: 20,
+  },
+  counter: {
+    fontSize: 16,
+    paddingRight: 20,
+  },
+  buttonWrapper: {
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    backgroundColor: '#F1E9F5',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    width: 150,
+  },
+});
+
+export default Pomodoro3;
