14646add5137c2d687243b586aff6c76a1fe2af9
Pushkar Singh <theagilecoder@gmail.com>
2020-07-28 18:06:22 +0530

    Edit Header, Cleanup App.js, Remove About

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View, ScrollView, StyleSheet} from 'react-native';
-import PushNotification from 'react-native-push-notification';
 import Header from './src/components/Header';
 import Pomodoro1 from './src/components/Pomodoro1';
 import Pomodoro2 from './src/components/Pomodoro2';
@@ -8,23 +7,6 @@ import Pomodoro3 from './src/components/Pomodoro3';
 import Pomodoro4 from './src/components/Pomodoro4';
 import Pomodoro5 from './src/components/Pomodoro5';
 import Pomodoro6 from './src/components/Pomodoro6';
-import About from './src/components/About';
-
-PushNotification.configure({
-  onRegister: function (token) {
-    console.log('TOKEN:', token);
-  },
-  onNotification: function (notification) {
-    console.log('NOTIFICATION:', notification);
-  },
-  permissions: {
-    alert: true,
-    badge: true,
-    sound: true,
-  },
-  popInitialNotification: true,
-  requestPermissions: Platform.OS === 'ios',
-});
 
 const App = () => {
   return (
@@ -37,7 +19,6 @@ const App = () => {
         <Pomodoro4 />
         <Pomodoro5 />
         <Pomodoro6 />
-        <About />
       </ScrollView>
     </View>
   );
@@ -46,9 +27,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import {Text, StyleSheet} from 'react-native';
 const Header = () => {
   return (
     <Text style={styles.text}>
+      <Text style={{fontSize: 25}}>1 Page </Text>
       <Text style={{color: 'blue'}}>P</Text>
       <Text style={{color: 'red'}}>o</Text>
       <Text style={{color: 'orange'}}>m</Text>
@@ -12,7 +13,6 @@ const Header = () => {
       <Text style={{color: 'red'}}>o</Text>
       <Text style={{color: 'orange'}}>r</Text>
       <Text style={{color: 'green'}}>o</Text>
-      <Text style={{fontSize: 20}}> for Smart Bands</Text>
     </Text>
   );
 };
