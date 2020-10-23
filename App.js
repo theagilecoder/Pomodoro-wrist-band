49820c3b85db9173754473882261b76bf00025d4
Pushkar Singh <theagilecoder@gmail.com>
2020-10-23 14:34:23 +0530

    Implement Tab navigation in Home

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import {View, ScrollView, StyleSheet} from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// Custom Components
 import Header from './src/components/Header';
 import Pomodoro1 from './src/components/Pomodoro1';
 import Pomodoro2 from './src/components/Pomodoro2';
@@ -8,9 +12,10 @@ import Pomodoro4 from './src/components/Pomodoro4';
 import Pomodoro5 from './src/components/Pomodoro5';
 import Pomodoro6 from './src/components/Pomodoro6';
 
-const App = () => {
+// 0.5 Hour Pomodoros Screen
+const ShortPomodoros = () => {
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1}}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Header />
         <Pomodoro1 />
@@ -24,10 +29,28 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+// 1 Hour Pomodoros Screen
+const LongPomodoros = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Long Pomodoros!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator 
+        tabBarOptions={{labelStyle: {fontSize: 15,}, tabStyle: {justifyContent: 'center'}}}
+      >
+        <Tab.Screen name="Short" component={ShortPomodoros} options={{tabBarLabel: "0.5 Hour Pomodoros"}} />
+        <Tab.Screen name="Long" component={LongPomodoros} options={{tabBarLabel: "1 Hour Pomodoros"}} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
